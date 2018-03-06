f0c1f827d0c56b5235d28fd32461bb50255e6495
Enzo <e.amespizarro@gmail.com>
2018-03-05 22:25:38 -0500

    create division search bar

--- src/components/ManagePage/CreateDivisionForm.js
+++ src/components/ManagePage/CreateDivisionForm.js
@@ -5,14 +5,42 @@ import { RenderInput, RenderButton } from 'utils/renderform';
 import { Link } from 'react-router';
 import { hasValue } from '../../utils/utilfunctions';
 import { Button, Divider, Transition } from 'semantic-ui-react';
+import { Search, Header } from 'semantic-ui-react';
+import { source } from '../../utils/dummydata';
+import _ from 'lodash';
+
 
 class CreateDivisionForm extends Component{
   constructor(props) {
     super(props);
     this.state = { 
       selectTeam: false,
-      inviteTeam: false 
+      inviteTeam: false,
+      isLoading: false, 
+      results: [],
+      value: ''
     }
+  }
+
+  resetComponent = () => {
+    this.setState({ isLoading: false, results: [], value: '' })
+  }
+
+  handleResultSelect = (e, { result }) => { 
+    this.setState({ value: result.title }) 
+  }
+
+  handleSearchChange = (e, { value }) => {
+    this.setState({ isLoading: true, value })
+
+    setTimeout(() => {
+      if (this.state.value.length < 1) return this.resetComponent()
+
+      const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
+      const isMatch = (result) => re.test(result.title)
+
+      this.setState({ isLoading: false, results: _.filter(source, isMatch), })
+    }, 500)
   }
 
   toggleVisibility = (e, props) => {
@@ -24,14 +52,25 @@ class CreateDivisionForm extends Component{
     }
   }
 
-  handleSubmit = (values) =>{
-    //values.preventDefault()
+  handleSubmit = (e, values) => {
+    e.preventDefault();
   }
 
   render(){
     const { fields: { divisionName }, handleSubmit, error, serverError} = this.props;
-    const { selectTeam, inviteTeam } = this.state 
+    const { selectTeam, inviteTeam } = this.state;
+    const { isLoading, value, results } = this.state;
+
     const mdsm_12 = "col-sm-12 col-md-12";
+
+
+    const renderResults = ({ title, description, season }) => [
+      <div key='content' className='content'>
+        {title && <div className='title'>{title}</div>}
+        {description && <div className='description'>{description}</div>}
+        {season && <div>{season}</div>}
+      </div>,
+    ]
 
     // ==========================
     // ====== SERVER ERROR ======
@@ -57,15 +96,20 @@ class CreateDivisionForm extends Component{
 
     const SelectTeam = () => {
       return(
-        <div className="col-md-12 padding-zero">
-          <div className="col-md-4 pop-genie padding-15">
-            <span className="text-label">Select your teams</span>
-            <Button content={selectTeam ? 'Hide' : 'Show'} onClick={this.toggleVisibility} icon="add" value={0}/>
+        <div className="col-md-12 pop-genie padding-zero">
+          <div className="col-md-12 padding-15">
+            <span className="text-label"> Select teams from anywhere in your league </span>
+            <Search loading={isLoading} onResultSelect={this.handleResultSelect} onSearchChange={this.handleSearchChange} results={results} value={value} resultRenderer={renderResults} {...this.props} />
           </div>
 
-          <Transition visible={selectTeam} animation='slide down' duration={400}>
-            <div className="col-md-12 pop-genie">SELECT TEAMS HERE</div>
-          </Transition>
+          
+          <div className="col-md-12">
+            <Header>State</Header>
+            <pre>{JSON.stringify(this.state, null, 2)}</pre>
+            {/*
+            <Header>Options</Header>
+            <pre>{JSON.stringify(source, null, 2)}</pre>*/}
+          </div>
         </div>
       )
     }
@@ -116,3 +160,68 @@ export default reduxForm({
   forceUnregisterOnUnmount: true
 })(CreateDivisionForm)
 
+
+
+
+
+
+
+
+
+// const source = _.times(5, () => ({
+//   title: faker.company.companyName(),
+//   description: faker.company.catchPhrase(),
+//   image: faker.internet.avatar(),
+//   price: faker.finance.amount(0, 100, 2, '$'),
+// }))
+
+// export default class SearchExampleStandard extends Component {
+//   componentWillMount() {
+//     this.resetComponent()
+//   }
+
+//   resetComponent = () => this.setState({ isLoading: false, results: [], value: '' })
+
+//   handleResultSelect = (e, { result }) => this.setState({ value: result.title })
+
+//   handleSearchChange = (e, { value }) => {
+//     this.setState({ isLoading: true, value })
+
+//     setTimeout(() => {
+//       if (this.state.value.length < 1) return this.resetComponent()
+
+//       const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
+//       const isMatch = result => re.test(result.title)
+
+//       this.setState({
+//         isLoading: false,
+//         results: _.filter(source, isMatch),
+//       })
+//     }, 500)
+//   }
+
+//   render() {
+//     const { isLoading, value, results } = this.state
+
+//     return (
+//       <Grid>
+//         <Grid.Column width={8}>
+//           <Search
+//             loading={isLoading}
+//             onResultSelect={this.handleResultSelect}
+//             onSearchChange={this.handleSearchChange}
+//             results={results}
+//             value={value}
+//             {...this.props}
+//           />
+//         </Grid.Column>
+//         <Grid.Column width={8}>
+//           <Header>State</Header>
+//           <pre>{JSON.stringify(this.state, null, 2)}</pre>
+//           <Header>Options</Header>
+//           <pre>{JSON.stringify(source, null, 2)}</pre>
+//         </Grid.Column>
+//       </Grid>
+//     )
+//   }
+// }
---
+++ src/utils/dummydata.js
@@ -0,0 +1,136 @@
+export const source = [
+  {
+    title: 'Division Gold',
+    description: 'the best team',
+    season: 'Summer Season',
+    type: 'division'
+  },
+  {
+    title: 'Division Premier',
+    description: 'the best team',
+    season: 'Summer Season',
+    type: 'division'
+  },
+  {
+    title: 'Division Bronze',
+    description: 'the best team',
+    season: 'Summer Season',
+    type: 'division'
+  },
+  {
+    title: 'Division Coed',
+    description: 'the best team',
+    season: 'Summer Season',
+    type: 'division'
+  },
+  {
+    title: 'Division Coed 2',
+    description: 'the best team',
+    season: 'Summer Season',
+    type: 'division'
+  },
+  {
+    title: 'Team Eagles',
+    description: 'the best team',
+    season: 'Summer Season',
+    type: 'team'
+  },
+  {
+    title: 'Team Tiger',
+    description: 'the best team',
+    season: 'Summer Season',
+    type: 'team'
+  },
+  {
+    title: 'Team Blue',
+    description: 'the best team',
+    season: 'Summer Season',
+    type: 'team'
+  },
+  {
+    title: 'Team Red',
+    description: 'the best team',
+    season: 'Summer Season',
+    type: 'team'
+  },
+  {
+    title: 'Team Black',
+    description: 'the best team',
+    season: 'Summer Season',
+    type: 'team'
+  },
+  {
+    title: 'Team White',
+    description: 'the best team',
+    season: 'Summer Season',
+    type: 'team'
+  },
+  {
+    title: 'Team Panther',
+    description: 'the best team',
+    season: 'Summer Season',
+    type: 'team'
+  },
+  {
+    title: 'Team Crown',
+    description: 'the best team',
+    season: 'Summer Season',
+    type: 'team'
+  },
+  {
+    title: 'Enzo Ames',
+    description: 'the best athlete',
+    season: 'Summer Season',
+    type: 'Athlete'
+  },
+  {
+    title: 'Kris Sighn',
+    description: 'the best athlete',
+    season: 'Summer Season',
+    type: 'Athlete'
+  },
+  {
+    title: 'Akbar Mirza',
+    description: 'the best athlete',
+    season: 'Summer Season',
+    type: 'Athlete'
+  },
+  {
+    title: 'Abu Butt',
+    description: 'the best athlete',
+    season: 'Summer Season',
+    type: 'Athlete'
+  },
+  {
+    title: 'Jay AbrahamJay',
+    description: 'the best athlete',
+    season: 'Summer Season',
+    type: 'Athlete'
+  }
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
