4e76fbf473c73bf7a0be2743c19d3f30d08157dd
Abu Butt <butabu@yahoo.com>
2018-03-11 18:16:47 -0400

    Modified Athlete Profile Page

--- src/containers/Profile/Profile.js
+++ src/containers/Profile/Profile.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
-import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { ProfileCard } from 'components';
 import { load } from '../../actions/Auth/actions';
 import { UserDetails } from 'containers';
-import { Card, Icon, Message, } from 'semantic-ui-react'
+import { Message, Advertisement, List, Header } from 'semantic-ui-react'
 
 class Home extends Component {
   constructor(props) {
     super(props);
   }
-
   componentWillMount(){
     if (this.props.params.username){
-      alert("VERFY ATHLETE PROFILE ACTION");
-    }
+      console.log("VERFY ATHLETE PROFILE USERNAME: " + this.props.params.username);
+      }
   }
-
   render() {
     const { auth } = this.props;
     const user = auth.user;
@@ -27,15 +24,14 @@ class Home extends Component {
       <div className="home">
         <Helmet title="Home"/>
         {user.credential ?
-          (<div className="container-fluid">
-            
-            
-            <div className="hidden-xs col-sm-3 col-md-8 col-lg-8">
+          (
+          <div className="container">
+            <div className="col-xs-12 col-sm-8 col-md-8 col-lg-8">
               <ProfileCard auth={auth} />
             </div>
           
             {/* Message */}
-            <div className="hidden-xs col-sm-3 col-md-4 col-lg-4">
+            <div className="hidden-xs col-sm-4 col-md-4 col-lg-4">
               <div className="pop-genie">
               <Message>
               <Message.Header>New Messages</Message.Header>
@@ -45,27 +41,32 @@ class Home extends Component {
               </Message.List>
             </Message>
               </div>
+
+              <div className="advertisement hidden-xs hidden-sm hidden-md col-lg-3">  
+              <h3>Advertisement</h3>
+              <Advertisement unit='medium rectangle' test='Ad' />
+              </div> 
+              
             </div>
-        
+            
 
           {/* Dashboard */}
-          <div className="hidden-xs col-sm-3 col-md-8 col-lg-8">
+          <div className="col-xs-12 col-sm-10 col-md-8 col-lg-8">
           <div className="pop-genie">
-          <h3 className="text-align">Your Dashboard</h3>
+          <Header as='h1'>Dashboard</Header>
+          <List>
+          <Header as='h2'><List.Item>History</List.Item></Header> <List.Description size="large">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec massa risus, interdum ac mauris vel, posuere consectetur tortor. Donec ultrices sem non augue hendrerit, gravida imperdiet felis pulvinar. Nam et risus sapien. Pellentesque et sem efficitur, consequat tortor vel, mattis risus. Donec rhoncus lacinia enim, sit amet venenatis risus venenatis ut. Sed ultricies ipsum at porta convallis.</List.Description>
+          <Header as='h2'><List.Item>Skills</List.Item></Header> <List.Description>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec massa risus, interdum ac mauris vel, posuere consectetur tortor. Donec ultrices sem non augue hendrerit, gravida imperdiet felis pulvinar. Nam et risus sapien. Pellentesque et sem efficitur, consequat tortor vel, mattis risus. Donec rhoncus lacinia enim, sit amet venenatis risus venenatis ut. Sed ultricies ipsum at porta convallis.</List.Description>
+          <Header as='h2'><List.Item>Connections</List.Item></Header><List.Description>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec massa risus, interdum ac mauris vel, posuere consectetur tortor. Donec ultrices sem non augue hendrerit, gravida imperdiet felis pulvinar. Nam et risus sapien. Pellentesque et sem efficitur, consequat tortor vel, mattis risus. Donec rhoncus lacinia enim, sit amet venenatis risus venenatis ut. Sed ultricies ipsum at porta convallis.</List.Description>
+          </List>
           </div>
           </div>
 
-
-            {/* <div className="hidden-xs col-sm-3 col-md-10">
-              <h3>{this.props.params.username}</h3>
-                <span className="text-muted">2 liner about athlete/team-captain</span>
-            </div> */}
-          
           </div>)
 
           : (<UserDetails />)
-        }
-      
+        
+      }
       </div>
     );
   }
@@ -80,27 +81,3 @@ const mapStateToProps = (state) => ({
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
-
-
-//with semantic card
-// import React from 'react'
-// import { Card, Icon } from 'semantic-ui-react'
-
-// const extra = (
-//   <a>
-//     <Icon name='user' />
-//     16 Friends
-//   </a>
-// )
-
-// const Profile = () => (
-//   <Card
-//     image='/assets/images/avatar/large/elliot.jpg'
-//     header= 'Abu Butt'
-//     meta='Friend'
-//     description='Abu Butt is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
-//     extra={extra}
-//   />
-// )
-
-// export default Profile
