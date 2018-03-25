2ab5ff434c6890214de3ee748553bec7c2a45ba8
enzoameso <e.amespizarro@gmail.com>
2018-03-25 16:21:48 +0000

    minor about page change

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -20,7 +20,7 @@ export default class About extends Component {
           <Photo className="col-xs-12 col-sm-6 col-md-6 about-photo" photoClassName="img-responsive golie-photo" src={flying_goalie} parentsHeight={""} />
           
           <div className="col-xs-12 col-sm-6 col-md-6">
-            <span className="inner-title">As an athlete find recreational leagues based on your preference or skill level</span>
+            <span className="inner-title">Find the nearest and best recreational league based on your preference</span>
             <p className="text-muted bottom-15">The features we provide for athletes are free of charge, we aim to build a community around sports. This idea originated from our love for sports
             and wanting everyone to fully enjoy the game. These features will aim to bring a sense of professionalism to the game, while keeping it very social
             and interactive</p>
@@ -103,8 +103,8 @@ export default class About extends Component {
               <div className="content">
                 <h2>2018</h2>
                 <h4>January | Research</h4>
-                <p>Throughout this month our research was divided into two parts. We reasearched the right development tools and technology to create our application.
-                We also performed customer resarch to better understand our business and potential users</p>
+                <p>Throughout this month our research was divided into two parts. We've reasearched the right development tools and technology to create our application.
+                Performed customer resarch to better understand our business and potential users</p>
               </div>
             </div>
             <div className="containertime right">
@@ -116,7 +116,7 @@ export default class About extends Component {
             <div className="containertime left">
               <div className="content">
                 <h4>March | Development</h4>
-                <p>This month is a cycle consisting of validating our customer reasearch and pivoting our startup into the right direction</p>
+                <p>We begin development of the platform and the goal is to achieve a minimum viable product by May</p>
               </div>
             </div>
             <div className="containertime right">
